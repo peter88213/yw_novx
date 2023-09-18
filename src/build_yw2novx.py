b8c4d2cc92b153bd23664bb4225efe2f8563d7bd
peter88213 <peter.88213@t-online.de>
2023-09-19 01:03:39 +0200

    Update build scripts

--- src/build_yw2novx.py
+++ src/build_yw2novx.py
@@ -1,24 +1,26 @@
 """ Build a python script for the yw2novx distribution.
         
 In order to distribute a single script without dependencies, 
-this script "inlines" all modules imported from the novxlib package.
+this script "inlines" all modules imported from the pywriter package.
 
-The novxlib project (see see https://github.com/peter88213/novxlib)
+The pywriter project (see see https://github.com/peter88213/PyWriter)
 must be located on the same directory level as the yw2novx project. 
 
 For further information see https://github.com/peter88213/yw2novx
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
+import sys
+sys.path.append('../../PyWriter/src')
 import inliner
 
 SRC = '../src/'
 BUILD = '../test/'
-SOURCE_FILE = f'{SRC}yw2novx_.pyw'
-TARGET_FILE = f'{BUILD}yw2novx.pyw'
+SOURCE_FILE = f'{SRC}yw2novx_.py'
+TARGET_FILE = f'{BUILD}yw2novx.py'
 
 
 def main():
-    inliner.run(SOURCE_FILE, TARGET_FILE, 'novxlib', '../../novxlib/src/')
+    inliner.run(SOURCE_FILE, TARGET_FILE, 'pywriter', '../../novxlib/src/')
     print('Done.')
 
 
