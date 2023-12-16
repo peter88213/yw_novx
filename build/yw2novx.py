f6947aeba770d9ad468b08b85a5503265605e0f2
peter88213 <peter.88213@t-online.de>
2023-12-16 21:20:31 +0100

    Set the copyright year to 2024

--- build/yw2novx.py
+++ build/yw2novx.py
@@ -1,6 +1,6 @@
 """Convert yw7 to novx.
 
-Copyright (c) 2023 Peter Triesberger
+Copyright (c) 2024 Peter Triesberger
 For further information see https://github.com/peter88213/yw2novx
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
--- src/upgrade_collection_.py
+++ src/upgrade_collection_.py
@@ -3,7 +3,7 @@
 - Convert a novelyst .nvcx collection file to .nvcx format.
 - Convert the collection's .yw7 project files to .novx format. 
 
-Copyright (c) 2023 Peter Triesberger
+Copyright (c) 2024 Peter Triesberger
 For further information see https://github.com/peter88213/
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
